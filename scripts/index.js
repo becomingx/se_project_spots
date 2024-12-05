05da2f4d0420f5ab43beb5d24b9a7817eb6734a4
Patricia Tirado <patriciatirado9@gmail.com>
2024-12-05 15:16:41 -0500

    Commit 1 project 4

--- scripts/index.js
+++ scripts/index.js
@@ -33,9 +33,18 @@ const initialCards = [
 const profileEditBtn = document.querySelector(".profile__edit-btn");
 const editModal = document.querySelector("#edit-modal");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
+
+
+
 
 
 function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal_opened");
 }
 
