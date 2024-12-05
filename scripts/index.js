1af151294c359f14ddbe2db231be8a170c021a8e
Patricia Tirado <patriciatirado9@gmail.com>
2024-12-05 16:35:25 -0500

    Commit 1 project 4

--- scripts/index.js
+++ scripts/index.js
@@ -31,16 +31,14 @@ const initialCards = [
 ];
 
 const profileEditBtn = document.querySelector(".profile__edit-btn");
-const editModal = document.querySelector("#edit-modal");
-const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
+
+const editModal = document.querySelector("#edit-modal");
+const editFormElement = editModal.querySelector(".modal__form");
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
-
-
-
-
 
 function openModal() {
   editModalNameInput.value = profileName.textContent;
@@ -52,6 +50,13 @@ function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
-profileEditBtn.addEventListener("click", openModal);
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  editModal.classList.remove("modal_opened");
+}
 
+profileEditBtn.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
