10fa0a975d2abdfb63a3ee68a14a24a1dbef5811
becomingx <patriciatirado9@gmail.com>
2025-02-21 18:29:32 -0500

    commit project 5

--- scripts/index.js
+++ scripts/index.js
@@ -57,7 +57,7 @@ const addModalNameInput = addModal.querySelector("#add-card-name-input");
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageElement = previewModal.querySelector(".modal__image");
 const previewModalCaption = previewModal.querySelector(".card__title");
-const previewModalCloseBtn = previewModal.querySelector(".preview__modal_close-btn");
+const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn--preview");
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
