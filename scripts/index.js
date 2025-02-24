64c0a7e666fdbed8bd72af3af9e8ac27c85eb6bc
becomingx <patriciatirado9@gmail.com>
2025-02-24 14:23:09 -0500

    Revert "commit project 5"
    This reverts commit 5b6c0e394e648c3ffbb96906c0b1cf668fa12c8e.

--- scripts/index.js
+++ scripts/index.js
@@ -154,9 +154,3 @@ initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
   cardsList.append(cardElement);
 });
-
-document.querySelectorAll('*').forEach(elem => {
-  if (elem.offsetWidth > document.documentElement.offsetWidth) {
-    console.log('Overflowing element:', elem);
-  }
-});
