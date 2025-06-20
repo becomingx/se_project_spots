86758e63f43b885d444a46ce0e762fcfbcd7c6ee
becomingx <patriciatirado9@gmail.com>
2025-06-19 22:12:21 -0400

    se_project_spots sprint 9 webpack added avatar

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      favicon: "./src/images/favicon.ico", // path to the favicon
+
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
